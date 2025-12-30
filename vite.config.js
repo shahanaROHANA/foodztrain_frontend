f194a91cc578511679527e77d7168cb1b6c13edb
shahanaROHANA <kokilarubanshakana@gmail.com>
2025-12-30 13:52:39 +0530

    fix vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,34 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// import path from 'path'
+// import { fileURLToPath } from 'url'
+
+// const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 5173,
+//     host: true,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:4004',
+//         changeOrigin: true,
+//       },
+//       '/webhook': {
+//         target: 'http://localhost:4004',
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
@@ -5,9 +36,9 @@ import { fileURLToPath } from 'url'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Important for correct asset paths on Vercel
   server: {
     port: 5173,
     host: true,
